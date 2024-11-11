52c438b8f88da0982a6c3c737b0795c01d56167b
samsul <samsul.marif100@gmail.com>
2024-11-11 12:10:12 +0800

    user middleware
    - custom response if errors and success

--- application/app/Http/Middleware/UserMiddleware.php
+++ application/app/Http/Middleware/UserMiddleware.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Middleware;
 
+use App\Http\Controllers\Controller;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
-class UserMiddleware
+class UserMiddleware extends Controller
 {
     /**
      * Handle an incoming request.
@@ -19,7 +20,7 @@ class UserMiddleware
     {
         //roleID 1 karena untuk user dan platform user adalah mobile
         if (Auth::guard()->user()->role_id != 1) {
-            return response()->json(['statusCode' => 401, 'message' => 'anda bukan user'], 401);
+            return $this->error_response('Your not users,this feature for user roles', 401, true, 'hint: please use token from role_id:1 cause that roles is users', env('APP_ENV'));
         }
 
         if (!empty($request->header('X-PLATFORM-NASHO')) && !empty($request->header('X-VERSION-NASHO')) && !empty($request->header('X-CLIENT-KEY-NASHO'))) {
@@ -28,7 +29,7 @@ class UserMiddleware
                     ->where('platform', $request->header('X-PLATFORM-NASHO'))
                     ->first()
             ) {
-                return response()->json(['statusCode' => 401, 'message' => 'platform not exists'], 401);
+                return $this->error_response('platform not exists', 401, true, 'Hint: platform available is mobile or web,and use that platform base on roles', env('APP_ENV'));
             }
 
             if (
@@ -36,7 +37,7 @@ class UserMiddleware
                 ->where('platform', $request->header('X-PLATFORM-NASHO'))
                 ->first()->platform != 'mobile'
             ) {
-                return response()->json(['statusCode' => 401, 'message' => 'wrong platform'], 401);
+                return $this->error_response('wrong platform', 401, true, 'Hint:use mobile platfrom cause that is user roles', env('APP_ENV'));
             }
 
             if (
@@ -46,7 +47,7 @@ class UserMiddleware
                         ['version', '=', $request->header('X-VERSION-NASHO')]
                     ])->first()
             ) {
-                return response()->json(['statusCode' => 401, 'message' => 'version platform tidak sesuai'], 401);
+                return $this->error_response('wrong version', 401, true, 'Hint: use the version platform is matches from platform used it', env('APP_ENV'));
             }
 
             if (
@@ -57,11 +58,11 @@ class UserMiddleware
                         ['client_key', '=', $request->header('X-CLIENT-KEY-NASHO')]
                     ])->first()
             ) {
-                return response()->json(['statusCode' => 401, 'message' => 'client key dari platform dan version tidak sesuai'], 401);
+                return $this->error_response('wrong client key', 401, true, 'Hint: please contact administrator for client key base on platform and version,cause that secret on used', env('APP_ENV'));
             }
             return $next($request);
         } else {
-            return response()->json(['statusCode' => 401, 'Message' => 'Header is:version,platform,client_key'], 401);
+            return $this->error_response('Please field all header it used', 401, true, 'Hint:version,platform,client_key', env('APP_ENV'));
         }
     }
 }
