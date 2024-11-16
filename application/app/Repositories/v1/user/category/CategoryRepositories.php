e05cf52764cd52d8cbb5a01ac8e5d5a5e325c7a5
samsul <samsul.marif100@gmail.com>
2024-11-16 09:25:46 +0800

    category
    - list
    - list materi by category

---
+++ application/app/Repositories/v1/user/category/CategoryRepositories.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories\v1\user\category;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class CategoryRepositories extends Controller
+{
+    private $category, $materi;
+    public function __construct()
+    {
+        $this->category = DB::table('kategori_materi');
+        $this->materi = DB::table('materi');
+    }
+
+    public function ListCategory()
+    {
+        return $this->category->get();
+    }
+
+    public function ListMateriByCategory($kategori_id)
+    {
+        $validate_category_id_for_materi = $this->handle_validate_category_id_for_materi($kategori_id);
+        if (!$validate_category_id_for_materi) {
+            return $this->error_response('Category not found', 422);
+        }
+
+        if ($validate_category_id_for_materi) {
+            return $this->success_response($this->materi->where('kategori_materi_id', $kategori_id)->get(), 'Successfully Get Materi By Category');
+        }
+    }
+
+    private function handle_validate_category_id_for_materi($kategori_id): bool
+    {
+        if ($this->materi->where('kategori_materi_id', $kategori_id)->first()) {
+            return true;
+        }
+        return false;
+    }
+}
