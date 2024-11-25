178dda28e0807ca3f5d6b5898be0ac721c862574
samsul <samsul.marif100@gmail.com>
2024-11-25 15:42:16 +0800

    unique materi_judul cancel

--- application/app/Http/Requests/v1/admin/master_data/MateriRequest.php
+++ application/app/Http/Requests/v1/admin/master_data/MateriRequest.php
@@ -23,7 +23,7 @@ class MateriRequest extends FormRequest
     {
         return [
             'embed' => 'required|string',
-            'judul' => 'required|string|unique:materi,judul',
+            'judul' => 'required|string',
             'kategori_materi_id' => 'string',
             'permalink' => 'string',
             'isi' => 'required|string',
