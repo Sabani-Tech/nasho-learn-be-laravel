e77a9fa9dc0ac37ff9145190a21437a08142696a
samsul <samsul.marif100@gmail.com>
2024-11-25 08:50:10 +0800

    show data relation to category from materi base on category

--- application/app/Repositories/v1/user/category/CategoryRepositories.php
+++ application/app/Repositories/v1/user/category/CategoryRepositories.php
@@ -3,15 +3,41 @@
 namespace App\Repositories\v1\user\category;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Facades\DB;
 
+//model
+class Materi extends Model
+{
+    protected $table = 'materi';
+    protected $fillable = ['embed', 'judul', 'permalink', 'isi', 'kategori_materi_id', 'created_at', 'updated_at'];
+
+    protected $casts = ['id' => 'string', 'created_at' => 'date:d-M-y H:i:s', 'updated_at' => 'date:d-M-y H:i:s'];
+    protected $hidden = ['kategori_materi_id'];
+
+    public function kategori(): HasOne
+    {
+        return $this->hasOne(Category::class, 'id', 'kategori_materi_id');
+    }
+}
+
+class Category extends Model
+{
+    protected $table = 'kategori_materi';
+    protected $fillable = ['jenis', 'deskripsi', 'created_at', 'updated_at'];
+
+    protected $casts = ['id' => 'string', 'created_at' => 'date:d-M-y H:i:s', 'updated_at' => 'date:d-M-y H:i:s'];
+}
+
+//repositories
 class CategoryRepositories extends Controller
 {
     private $category, $materi;
     public function __construct()
     {
-        $this->category = DB::table('kategori_materi');
-        $this->materi = DB::table('materi');
+        $this->category = new Category();
+        $this->materi = new Materi();
     }
 
     public function ListCategory()
@@ -27,7 +53,7 @@ class CategoryRepositories extends Controller
         }
 
         if ($validate_category_id_for_materi) {
-            return $this->success_response($this->materi->where('kategori_materi_id', $kategori_id)->get(), 'Successfully Get Materi By Category');
+            return $this->success_response($this->materi->where('kategori_materi_id', $kategori_id)->with('kategori')->get(), 'Successfully Get Materi By Category');
         }
     }
 
