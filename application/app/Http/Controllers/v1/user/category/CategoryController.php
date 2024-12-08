2fddb95997a2cc3dfdf9e3be363081fe28718b6b
samsul <samsul.marif100@gmail.com>
2024-12-08 18:39:53 +0800

    improve: fix GET list materi BY category ID

--- application/app/Http/Controllers/v1/user/category/CategoryController.php
+++ application/app/Http/Controllers/v1/user/category/CategoryController.php
@@ -18,6 +18,11 @@ class CategoryController extends Controller
 
     public function ListMateriByCategory($kategori_id)
     {
-        return $this->success_response($this->categoryRepositories->ListMateriByCategory($kategori_id));
+        return $this->categoryRepositories->ListMateriByCategory($kategori_id);
+    }
+
+    public function UpdateStatusByCategoryId($category_id, $status_category)
+    {
+        return $this->categoryRepositories->UpdateStatusByCategoryId($category_id, $status_category);
     }
 }
---
+++ application/database/migrations/2024_12_08_160504_create_kategori_materi_detail_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kategori_materi_detail', function (Blueprint $table) {
+            $table->id();
+            $table->enum('exam1', ['false', 'true']);
+            $table->enum('exam2', ['false', 'true']);
+            $table->enum('status', ['Materi1', 'Exam1', 'Materi2', 'Exam2']);
+            $table->string('kategori_materi_id')->unique();
+            $table->foreignId('users_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kategori_materi_detail');
+    }
+};
