bd740d74e9ea1ba1245b9ff57f52d8e34e0c22e8
samsul <samsul.marif100@gmail.com>
2024-12-08 21:12:10 +0800

    migration: soal
    - id (uuid)
    - title
    - question
    - point
    - option (with json type)

---
+++ application/database/migrations/2024_12_08_200822_create_soal_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('soal', function (Blueprint $table) {
+            $table->uuid('id')
+                ->primary()
+                ->default(DB::raw('(UUID())'));
+            $table->string('title', 128);
+            $table->string('question', 128);
+            $table->integer('point');
+            $table->json('option');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('soal');
+    }
+};
