15e090935d50e954256c90da5eae835dd0a47003
samsul <samsul.marif100@gmail.com>
2024-12-16 16:35:31 +0800

    Quis: resource custom of show pembahasan

--- application/app/Http/Resources/PembahasanResource.php
+++ application/app/Http/Resources/PembahasanResource.php
@@ -15,12 +15,29 @@ class PembahasanResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        foreach (json_decode(DB::table('quis')->where('id', $this->quis_id)->first()->option, true) as $key) {
+            if ($key['key'] == DB::table('quis')->where('id', $this->quis_id)->first()->answer_key) {
+                $CollectOptionRightAnswer = $key['value'];
+            }
+        }
+        foreach (json_decode(DB::table('quis')->where('id', $this->quis_id)->first()->option, true) as $key) {
+            if ($key['key'] == $this->answer) {
+                $CollectOptionAnswer = $key['value'];
+            }
+        }
         return [
             'id' => $this->id,
             'qustion' => DB::table('quis')->where('id', $this->quis_id)->first()->question,
             'point' => $this->point,
-            'answer' => $this->answer,
-            'right_answer' => DB::table('quis')->where('id', $this->quis_id)->first()->answer_key,
+            'answer' => array(
+                'key' => $this->answer,
+                'value' => $CollectOptionAnswer,
+            ),
+            'right_answer' => array(
+                'key' =>  DB::table('quis')->where('id', $this->quis_id)->first()->answer_key,
+                'value' => $CollectOptionRightAnswer,
+            ),
+            // 'right_answer' => DB::table('quis')->where('id', $this->quis_id)->first()->answer_key,
         ];
     }
 }
