f3f33f105b7a11f9a10706a0168092271012126e
samsul <samsul.marif100@gmail.com>
2024-12-19 16:55:10 +0800

    ExamPembahasan
    - show exam result to pembahasan

--- application/app/Http/Resources/PembahasanExamResource.php
+++ application/app/Http/Resources/PembahasanExamResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class PembahasanExamResource extends JsonResource
@@ -14,6 +15,29 @@ class PembahasanExamResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        foreach (json_decode(DB::table('exam')->where('id', $this->exam_id)->first()->option, true) as $key) {
+            if ($key['key'] == DB::table('exam')->where('id', $this->exam_id)->first()->answer_key) {
+                $CollectOptionRightAnswer = $key['value'];
+            }
+        }
+        foreach (json_decode(DB::table('exam')->where('id', $this->exam_id)->first()->option, true) as $key) {
+            if ($key['key'] == $this->answer) {
+                $CollectOptionAnswer = $key['value'];
+            }
+        }
+        return [
+            'id' => $this->exam_id,
+            'question' => DB::table('exam')->where('id', $this->exam_id)->first()->question,
+            'point' => $this->point,
+            'answer' => array(
+                'key' => $this->answer,
+                'value' => $CollectOptionAnswer,
+            ),
+            'right_answer' => array(
+                'key' =>  DB::table('exam')->where('id', $this->exam_id)->first()->answer_key,
+                'value' => $CollectOptionRightAnswer,
+            ),
+            // 'right_answer' => DB::table('exam')->where('id', $this->exam_id)->first()->answer_key,
+        ];
     }
 }
