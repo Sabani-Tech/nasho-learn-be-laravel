8fe762c2d3204dd73d69792a636aa3a09be800dd
samsul <samsul.marif100@gmail.com>
2024-12-20 08:56:11 +0800

    Materi
    - fix type use of phase with (int)
    - fix type use of quis_status with (bool)

--- application/app/Http/Resources/MateriDetailResource.php
+++ application/app/Http/Resources/MateriDetailResource.php
@@ -18,11 +18,11 @@ class MateriDetailResource extends JsonResource
         return [
             "id" => $this->id,
             "embed" => $this->embed,
-            "phase" => $this->phase,
+            "phase" => (int)$this->phase,
             "judul" => $this->judul,
             "permalink" => $this->permalink,
             "file_uri" => url("assets/{$this->file_uri}"),
-            "quis_status" => $this->quis_status,
+            "quis_status" => (bool)$this->quis_status,
             "urutan" => $this->urutan,
             "kategori_materi_id" => DB::table('kategori_materi')
                 ->whereId($this->kategori_materi_id)
