b96b97f417807c050f523a8f6638e5ca1c1eed0a
samsul <samsul.marif100@gmail.com>
2024-12-24 07:37:26 +0800

    Bug:soal[exam]
    - position of commit after update exam status and status lock

--- application/app/Repositories/v1/user/soal/SoalRepositories.php
+++ application/app/Repositories/v1/user/soal/SoalRepositories.php
@@ -323,7 +323,6 @@ class SoalRepositories extends Controller
 
             //submit exam
             DB::table('exam_answer')->insert($this->_SetRequestExamSubmit($category_id, $REQUEST_POST, $REQUEST_GET_PHASE));
-            DB::commit();
             // update status after passed exam phase 1 (uts) or exam phase 2 (uas)
             if (
                 $this->HandleMappingSubmitExam($category_id, $REQUEST_GET_PHASE)['score'] >= 60 &&
@@ -338,7 +337,7 @@ class SoalRepositories extends Controller
                         break;
                 }
             }
-
+            DB::commit();
             //return mapping exam
             return $this->success_response($this->HandleMappingSubmitExam($category_id, $REQUEST_GET_PHASE));
         } catch (\Exception $e) {
