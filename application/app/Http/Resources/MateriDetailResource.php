709020d18fb30ab92df6822f9d05e00e7e38a90e
samsul <samsul.marif100@gmail.com>
2024-12-27 08:21:57 +0800

    MateriDetail:query_builder do not used

--- application/app/Http/Resources/MateriDetailResource.php
+++ application/app/Http/Resources/MateriDetailResource.php
@@ -5,7 +5,6 @@ namespace App\Http\Resources;
 use App\Models\CategoryMateri;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\DB;
 
 class MateriDetailResource extends JsonResource
 {
