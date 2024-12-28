c098d41841ca64428605ef632f2ba4022209df3d
samsul <samsul.marif100@gmail.com>
2024-12-28 08:04:45 +0800

    MasterData: quis

--- application/app/Models/QuisModel.php
+++ application/app/Models/QuisModel.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class QuisModel extends Model
 {
     protected $table = 'quis';
+    protected $fillable = ['title', 'question', 'point', 'option', 'kategori_materi_id', 'materi_id', 'created_at', 'updated_at'];
     protected $casts = [
         'option' => 'array',
         'id' => 'string',
