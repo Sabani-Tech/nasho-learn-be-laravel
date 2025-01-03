4296ca3e197c38654e7049d115cdbf95e95478cb
samsul <samsul.marif100@gmail.com>
2025-01-03 16:06:11 +0800

    hotfix: Cred header
    - fix header use of stagging
    - fix header use of production of release

---
+++ application/database/seeders/HeaderReqSeederProduction.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class HeaderReqSeederProduction extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('request_header')->insert([[
+            'platform' => 'web-prod',
+            'client_key' => Str::uuid(),
+            'version' => '1.0',
+            'created_at' => Carbon::now()->timezone(env('APP_TIMEZONE'))->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->timezone(env('APP_TIMEZONE'))->format('Y-m-d H:i:s'),
+        ], [
+            'platform' => 'internal-tools-prod',
+            'client_key' => Str::uuid(),
+            'version' => '1.0',
+            'created_at' => Carbon::now()->timezone(env('APP_TIMEZONE'))->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->timezone(env('APP_TIMEZONE'))->format('Y-m-d H:i:s'),
+        ], [
+            'platform' => 'mobile-prod',
+            'client_key' => Str::uuid(),
+            'version' => '1.0',
+            'created_at' => Carbon::now()->timezone(env('APP_TIMEZONE'))->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->timezone(env('APP_TIMEZONE'))->format('Y-m-d H:i:s'),
+        ]]);
+    }
+}
--- application/database/seeders/HeaderReqSeederStagging.php
+++ application/database/seeders/HeaderReqSeederStagging.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-class HeaderReqSeeder extends Seeder
+class HeaderReqSeederStagging extends Seeder
 {
     /**
      * Run the database seeds.
