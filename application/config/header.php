2d2047a78a6fda7ac20d28572d8be50f8dcd70b4
samsul <samsul.marif100@gmail.com>
2025-01-03 17:08:33 +0800

    create config of cred header
    - stagging
    - release(production)

---
+++ application/config/header.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'mobile_platform' => env('X_PLATFORM_NASHO_MOBILE'),
+    'admin_platform' => env('X_PLATFORM_NASHO_ADMIN'),
+    'internal_tools_platform' => env('X_PLATFORM_NASHO_INTERNAL_TOOLS'),
+];
